b2b483378b1525161c2f55cd17f093f7c81d6814
kowshik-noor <kowshik.noor@gmail.com>
2021-08-27 12:35:19 -0400

    attempted to make an algorithm for DojoSweeper

---
+++ WebFundamentals/Algorithms/DojoSweeper/app.js
@@ -0,0 +1,89 @@
+var theDojo = [ [1, 0, 1, 1, 1, 0, 4, 0, 8, 0],
+                [3, 1, 0, 7, 0, 0, 6, 0, 8, 8],
+                [5, 0, 7, 0, 3, 6, 6, 6, 0, 0],
+                [2, 3, 0, 9, 0, 0, 6, 0, 8, 0],
+                [6, 0, 3, 3, 0, 2, 0, 3, 0, 4],
+                [0, 0, 3, 3, 0, 0, 2, 2, 3, 0],
+                [0, 0, 0, 0, 5, 0, 1, 2, 0, 6],
+                [2, 2, 2, 2, 0, 7, 1, 1, 1, 0],
+                [5, 2, 0, 2, 0, 0, 0, 1, 1, 2],
+                [9, 2, 2, 2, 0, 7, 0, 1, 1, 0] ];
+var dojoDiv = document.querySelector("#the-dojo");
+    
+// Creates the rows of buttons for this game
+function render(theDojo) {
+  var result = "";
+  for(var i=0; i<theDojo.length; i++) {
+    for(var j=0; j<theDojo[i].length; j++) {
+      result += `<button class="tatami" onclick="howMany(${i}, ${j}, this)"></button>`;
+    }
+  }
+  return result;
+}
+    
+// TODO - Make this function tell us how many ninjas are hiding 
+//        under the adjacent (all sides and corners) squares.
+//        Use i and j as the indexes to check theDojo.
+function howMany(i, j, element) {
+    console.log({i, j});
+    // alert("TODO - determine how many ninjas are hiding in adjacent squares");
+    
+    let top;
+    var topright;
+    let bottomright;
+    let topleft;
+    let bottomleft;
+
+    if (i - 1 < 0) {
+        top = 0
+        topright = 0
+        topleft = 0;
+    } else {
+        top = theDojo[i - 1][j]
+    }
+    
+
+    let right = theDojo[i][j + 1];
+
+    let bottom;
+    if (!i + 1) {
+        bottom = 0;
+        bottomright = 0;
+        bottomleft = 0;
+    } else {
+        theDojo[i + 1][j];
+    }
+
+    let left = theDojo[i][j - 1];
+
+    if (!right) {
+        right = 0;
+        topright = 0;
+    }
+
+
+    if (j-1<0) {
+        left = 0;
+    }
+
+
+    element.innerText = top + topright + topleft + bottomleft + left + bottom + bottomright + right;
+
+}
+    
+// BONUS CHALLENGES
+// 1. draw the number onto the button instead of alerting it
+// 2. at the start randomly place 10 ninjas into theDojo with at most 1 on each spot
+// 3. if you click on a ninja you must restart the game 
+//    dojoDiv.innerHTML = `<button onclick="location.reload()">restart</button>`;
+    
+// start the game
+// message to greet a user of the game
+var style="color:cyan;font-size:1.5rem;font-weight:bold;";
+console.log("%c" + "IF YOU ARE A DOJO STUDENT...", style);
+console.log("%c" + "GOOD LUCK THIS IS A CHALLENGE!", style);
+// shows the dojo for debugging purposes
+console.table(theDojo);
+// adds the rows of buttons into <div id="the-dojo"></div> 
+dojoDiv.innerHTML = render(theDojo);    
+
