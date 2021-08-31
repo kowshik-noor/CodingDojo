37b74cf24eb9fe0ba47ed0c98f2136a12470d999
kowshik-noor <kowshik.noor@gmail.com>
2021-08-31 15:00:29 -0400

    added chaining methods

--- Python/fundamentals/oop/user.py
+++ Python/fundamentals/oop/user.py
@@ -5,9 +5,11 @@ class User:
 
     def make_deposit(self, amount):
         self.balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.balance -= amount
+        return self
 
     def display_user_balance(self):
         print(f"User: {self.name}, Balance: ${self.balance}")
@@ -15,29 +17,17 @@ class User:
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
 
 kowshik = User("Kowshik Noor")
 sadia = User("Kshboo Sadia")
 karen = User("Karen Subha")
 
-kowshik.make_deposit(400)
-kowshik.make_deposit(1200)
-kowshik.make_deposit(1400)
-kowshik.make_withdrawal(400)
-kowshik.display_user_balance()
+kowshik.make_deposit(400).make_deposit(1200).make_deposit(1400).make_withdrawal(400).display_user_balance()
 
-sadia.make_deposit(20)
-sadia.make_deposit(150)
-sadia.make_withdrawal(100)
-sadia.make_withdrawal(5)
-sadia.display_user_balance()
+sadia.make_deposit(20).make_deposit(150).make_withdrawal(100).make_withdrawal(5).display_user_balance()
 
-karen.make_deposit(30)
-karen.make_withdrawal(5)
-karen.make_withdrawal(3)
-karen.make_withdrawal(1)
-karen.display_user_balance()
+karen.make_deposit(30).make_withdrawal(5).make_withdrawal(3).make_withdrawal(1).display_user_balance()
 
-kowshik.transfer_money(sadia, 20)
-kowshik.display_user_balance()
+kowshik.transfer_money(sadia, 20).display_user_balance()
 sadia.display_user_balance()
