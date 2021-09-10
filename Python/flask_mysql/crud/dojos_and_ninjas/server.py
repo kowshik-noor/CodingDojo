4d859567bf546ed2c7345ffb10a51a2fe7f95f88
kowshik-noor <kowshik.noor@gmail.com>
2021-09-10 18:16:12 -0400

    slight adjustment to dojos and ninjas

--- Python/flask_mysql/crud/dojos_and_ninjas/server.py
+++ Python/flask_mysql/crud/dojos_and_ninjas/server.py
@@ -4,4 +4,4 @@ from flask_app.controllers.ninjas import Ninja
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
