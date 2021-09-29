cc4ad4b6d0ca5be8083e905953dc68a7111eb9b7
kowshik-noor <kowshik.noor@gmail.com>
2021-09-29 15:57:53 -0400

    mammals and gorillas

---
+++ Java/oop/oopAdvanced/zooKeeper/Gorilla.java
@@ -0,0 +1,16 @@
+public class Gorilla extends Mammal {
+    public void throwSomething() {
+        System.out.println("The gorilla has thrown something.");
+        this.energyLevel -= 5;
+    }
+
+    public void eatBananas() {
+        System.out.println("The gorilla is satisfied.");
+        this.energyLevel += 10;
+    }
+
+    public void climb() {
+        System.out.println("The gorilla has climbed a tree.");
+        this.energyLevel -= 10;
+    }
+}
---
+++ Java/oop/oopAdvanced/zooKeeper/GorillaTest.java
@@ -0,0 +1,13 @@
+public class GorillaTest {
+    public static void main(String[] args) {
+        Gorilla kong = new Gorilla();
+        kong.displayEnergy();
+
+        kong.climb();
+        kong.eatBananas();
+        kong.throwSomething();
+
+        kong.displayEnergy();
+        
+    }
+}
