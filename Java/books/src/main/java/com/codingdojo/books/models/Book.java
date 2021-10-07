470e9f1acb4a50bc0669b707dc1ad60c759d75c6
Noor Kowshik <kowshik.noor@gmail.com>
2021-10-07 19:01:02 -0400

    books api assignment

---
+++ Java/books/src/main/java/com/codingdojo/books/models/Book.java
@@ -0,0 +1,113 @@
+package com.codingdojo.books.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+//create a table in our schema.
+@Entity
+@Table(name="books")
+public class Book {
+//	the id will automatically increment
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+//    this strings will not be null and have a certain length of characters
+    @NotNull
+    @Size(min = 5, max = 200)
+    private String title;
+    @NotNull
+    @Size(min = 5, max = 200)
+    private String description;
+    @NotNull
+    @Size(min = 3, max = 40)
+    private String language;
+//    a book will have a minimum of 100 pages
+    @NotNull
+    @Min(100)
+    private Integer numberOfPages;
+    // This will not allow the createdAt column to be updated after creation
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    
+    public Book() {
+    }
+    public Book(String title, String desc, String lang, int pages) {
+        this.title = title;
+        this.description = desc;
+        this.language = lang;
+        this.numberOfPages = pages;
+    }
+    
+//    getters and setters
+    
+    public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public Integer getNumberOfPages() {
+		return numberOfPages;
+	}
+	public void setNumberOfPages(Integer numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	
+//	this is so that the created at updatedAt will automatically update when you save. and the created at will be updated once when you create
+	@PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+}
---
+++ Java/books/src/main/java/com/codingdojo/books/services/BookService.java
@@ -0,0 +1,53 @@
+package com.codingdojo.books.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.books.models.Book;
+import com.codingdojo.books.repositories.BookRepository;
+
+@Service
+public class BookService {
+    // adding the book repository as a dependency
+    private final BookRepository bookRepository;
+    
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    // returns all the books
+    public List<Book> allBooks() {
+        return bookRepository.findAll();
+    }
+    // creates a book
+    public Book createBook(Book b) {
+        return bookRepository.save(b);
+    }
+    // retrieves a book
+    public Book findBook(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        if(optionalBook.isPresent()) {
+            return optionalBook.get();
+        } else {
+            return null;
+        }
+    }
+    
+    public void deleteBook(Long id) {
+    	bookRepository.deleteById(id);
+    }
+    
+    public Book updateBook(Long id, String title, String desc, String lang, Integer pages) {
+    	Book book = this.findBook(id);
+    	if (book == null) {
+    		return book;
+    	}
+    	book.setTitle(title);
+    	book.setDescription(desc);
+    	book.setLanguage(lang);
+    	book.setNumberOfPages(pages);
+    	bookRepository.save(book);
+    	return book;
+    }
+}
