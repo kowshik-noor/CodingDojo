b0d6ac71f77caed1a1435f9d88c8fdffdb769f49
Noor Kowshik <kowshik.noor@gmail.com>
2021-10-26 13:12:30 -0400

    first jsx assignment

---
+++ MERN/react/intro/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Master React</li>
+        <li>Finish this bootcamp</li>
+        <li>Get a job</li>
+        <li>Move to Florida</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
