013623df46ff17417f2988fb4f839c199fb7e559
Noor Kowshik <kowshik.noor@gmail.com>
2021-10-25 20:28:04 -0400

    super ninja assignment

--- MERN/Javascript/oop/ninja.js
+++ MERN/Javascript/oop/ninja.js
@@ -25,7 +25,28 @@ class Ninja {
     }
 }
 
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake().showStats()
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name)
+        super.health = 200
+        super.speed = 10
+        super.strength = 10
+        this.wisdom = 10
+    }
+
+    speakWisdom() {
+        super.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+// const ninja1 = new Ninja("Hyabusa");
+// ninja1.sayName();
+// ninja1.showStats();
+// ninja1.drinkSake().showStats()
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+// -> "What one programmer can do in one month, two programmers can do in two months."
+superSensei.showStats();
+// -> "Name: Master Splinter, Health: 210, Speed: 10, Strength: 10"
