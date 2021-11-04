0e1ce382dc77c0edac301a6edc2f970741ef01e2
Noor Kowshik <kowshik.noor@gmail.com>
2021-11-04 18:09:39 -0400

    axios pokemon api assignment

--- MERN/react/apis/pokemon-api/src/components/PokemonDisplay.jsx
+++ MERN/react/apis/pokemon-api/src/components/PokemonDisplay.jsx
@@ -1,14 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const PokemonDisplay = () => {
     const [poke, setPoke] = useState([])
 
-    const getPoke = async (e) => {
-        const res = await (await fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")).json()
-        console.log(res)
-        const pokeList = res.results.map((p) => p.name)
-        setPoke(pokeList)
+    // const getPoke = async (e) => {
+    //     const res = await (await fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")).json()
+    //     console.log(res)
+    //     const pokeList = res.results.map((p) => p.name)
+    //     setPoke(pokeList)
+    // }
+
+    const getPoke = (e) => {
+        axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
+            .then(res => {
+                const pokeList = res.data.results.map(p => p.name)
+                setPoke(pokeList)
+        })
     }
+
     return (
         <div>
             <button onClick={getPoke}>Fetch Pokemon</button>
